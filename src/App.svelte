6f91d40cbe75eb94cc4637fc7c001418e01223ad
madarsbiss <madars.biss@gmail.com>
2020-11-11 23:19:25 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Card from './Card.svelte';
+
+</script>
+
+<main>
+	<h1>Svelte Memory Card game</h1>
+	<Card title={"Madza"} image={"https://source.unsplash.com/random/300x200"}/>
+</main>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+
+	main {
+		max-width: 800px;
+		margin: 0 auto;
+	}
+</style>
