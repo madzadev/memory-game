eb4031fd1acfc334e4480b21ce1d1f9f87068846
madarsbiss <madars.biss@gmail.com>
2020-11-12 13:53:39 +0200

    refactored cards variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,31 @@
 <script>
   import Card from "./Card.svelte";
 
-  let first = "";
-  let second = "";
-
-  let firstNum = 0;
-  let secondNum = 0;
+  let firstChoice = "";
+  let secondChoice = "";
 
   let openCards = [];
 
   const compare = (a, b) => {
-    if (a === b) {
-      console.log("images match");
-      openCards.push(firstNum, secondNum);
-      console.log(`open array is: ${openCards}`);
+    if (a.currentSrc === b.currentSrc) {
+      openCards.push(parseInt(a.innerText), parseInt(b.innerText));
     }
   };
 
-  const reset = () => {
+  const reset = (a, b) => {
     setTimeout(() => {
-      first = "";
-      second = "";
-      firstNum = 0;
-      secondNum = 0;
+      a = "";
+      b = "";
     }, 1000);
   };
 
   const cardClickHandler = (e) => {
-    if (first == "") {
-      firstNum = e.target.innerText;
-      first = e.target.currentSrc;
+    if (firstChoice == "") {
+      firstChoice = e.target;
     } else {
-      secondNum = e.target.innerText;
-      second = e.target.currentSrc;
-      compare();
-      reset();
+      secondChoice = e.target;
+      compare(firstChoice, secondChoice);
+      reset(firstChoice, secondChoice);
     }
   };
 </script>
@@ -55,11 +46,11 @@
 
 <h1>Svelte Memory Card game</h1>
 <main>
-  {#each { length: 16 } as card, index}
+  {#each { length: 16 } as card, i}
     <Card
-      title={index + 1}
+      title={i + 1}
       image={'https://source.unsplash.com/random/200x150'}
       onClick={cardClickHandler}
-      active={openCards.includes(index + 1) || firstNum == index + 1 || secondNum == index + 1} />
+      active={openCards.includes(i + 1) || (firstChoice && firstChoice.innerText == i + 1) || (secondChoice && secondChoice.innerText == i + 1)} />
   {/each}
 </main>
