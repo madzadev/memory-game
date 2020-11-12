6e7fa368e4435908f1583d60e210adc17ac1650a
madarsbiss <madars.biss@gmail.com>
2020-11-12 10:05:09 +0200

    created grid layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,51 @@
 <script>
-import Card from './Card.svelte';
+  import Card from "./Card.svelte";
 
-let first = '';
-let second = '';
+  let first = "";
+  let second = "";
 
-const compare =(a, b)=>{
-	if(a===b){
-console.log('images match')
-	}
-}
+  const compare = (a, b) => {
+    if (a === b) {
+      console.log("images match");
+    }
+  };
 
-const cardClickHandler = (e) => {
-	if(first==''){
-first = e.target.currentSrc;
-	} else {
-second = e.target.currentSrc;
-compare();
-	}
-	
-}
+  const reset = () => {
+    first = "";
+    second = "";
+  };
 
-
-
+  const cardClickHandler = (e) => {
+    if (first == "") {
+      console.log(e);
+      first = e.target.currentSrc;
+    } else {
+      second = e.target.currentSrc;
+      compare();
+    }
+  };
 </script>
 
-<main>
-	<h1>Svelte Memory Card game</h1>
-	<Card title={"?"} image={"https://source.unsplash.com/random/300x200"} onClick={cardClickHandler}/>
-</main>
-
 <style>
-	h1 {
-		text-align: center;
-	}
+  h1 {
+    text-align: center;
+  }
 
-	main {
-		max-width: 800px;
-		margin: 0 auto;
-	}
+  main {
+    max-width: 1000px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+  }
 </style>
+
+<h1>Svelte Memory Card game</h1>
+<main>
+  {#each { length: 16 } as card, i}
+    <Card
+      title={'?'}
+      image={'https://source.unsplash.com/random/200x150'}
+      onClick={cardClickHandler} />
+  {/each}
+</main>
