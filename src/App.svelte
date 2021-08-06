2f5b984025ab2823f110111e2af31bb990316e35
madarsbiss <madars.biss@gmail.com>
2021-08-06 14:11:40 +0300

    pair cards fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,102 +1,134 @@
 <script>
-  import Card from "./Card.svelte";
+import Card from "./Card.svelte";
 
-  // Controls
-  const cards = 12;
-  const theme = "animals";
+const cards = 12;
+const theme = "nature";
 
-  //   Generate numbers Schwartzian transform method
-  let arr = [...Array(cards / 2).keys()]
-    .map((i) => i + 1)
-    .flatMap((i) => [i, i])
-    .map((a) => ({
+let firstChoice = "",
+   secondChoice = "",
+   openCards = [],
+   activeOpen = "",
+   message = "Open cards!",
+   guesses = 0,
+   matches = 0,
+   loading = false;
+
+const prepareArray = (cards) => [...Array(cards / 2).keys()]
+   .map((i) => i + 1)
+   .flatMap((i) => [i, i])
+   .map((a) => ({
       sort: Math.random(),
       value: a,
-    }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value);
+   }))
+   .sort((a, b) => a.sort - b.sort)
+   .map((a) => a.value);
 
-  let firstChoice = "";
-  let secondChoice = "";
+let arr = prepareArray(cards)
 
-  let openCards = [];
-
-  let message = "Open cards!";
-
-  let guesses = 0;
-  let matches = 0;
-
-  const compare = (a, b) => {
-    ++guesses;
-    if (
+const compare = (a, b) => {
+   loading = true;
+   ++guesses;
+   if (
       a.nextElementSibling.firstChild.currentSrc ===
       b.nextElementSibling.firstChild.currentSrc
-    ) {
+   ) {
       ++matches;
       openCards.push(parseInt(a.innerText), parseInt(b.innerText));
       if (cards / 2 == matches) {
-        message = "You won!";
+         message = "You won!";
       } else {
-        message = "Found a match!";
+         message = "Found a match!";
       }
-
       firstChoice = "";
       secondChoice = "";
-    } else {
+      loading = false;
+   } else {
       message = "No match!";
       reset();
-    }
-  };
+   }
+};
 
-  const reset = () => {
-    setTimeout(() => {
+const reset = () => {
+   setTimeout(() => {
       firstChoice = "";
       secondChoice = "";
-    }, 800);
-  };
+      loading = false;
+      activeOpen = "";
+   }, 800);
+};
 
-  const cardClickHandler = (e) => {
-    if(!firstChoice) {
-      firstChoice = e.target;
-    } else {
-      secondChoice = e.currentTarget.firstChild.firstChild;
-      if(firstChoice!==secondChoice) {
-        compare(firstChoice, secondChoice);
+const resetGame = async () => {
+
+   arr = prepareArray(cards);
+
+   const images = Array.from(document.getElementsByClassName('image'));
+
+   images.forEach((image, index) => {
+      image.src = `https://source.unsplash.com/random/260x200?${arr[index]}?${theme ? theme : ''}`
+   })
+
+   firstChoice = "",
+      secondChoice = "",
+      openCards = [],
+      message = "Open cards!",
+      guesses = 0,
+      matches = 0;
+}
+
+const cardClickHandler = (e) => {
+   const isOpened = openCards.includes(Number(e.currentTarget.firstChild.firstChild.innerText))
+   const isActiveOpen = Number(activeOpen) == Number(e.currentTarget.firstChild.firstChild.innerText);
+
+   e.preventDefault();
+   if (!loading && !isOpened && !isActiveOpen) {
+      if (!firstChoice) {
+         firstChoice = e.currentTarget.firstChild.firstChild;
       } else {
-        firstChoice='';
-        secondChoice='';
+         secondChoice = e.currentTarget.firstChild.firstChild;
+         compare(firstChoice, secondChoice);
       }
-    }
-  };
+   }
+
+   activeOpen = e.currentTarget.firstChild.firstChild.innerText;
+};
 </script>
 
 <style>
-  h1 {
-    text-align: center;
-  }
+h1 {
+   text-align: center;
+}
 
-  main {
-    max-width: 1200px;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));;
-    gap: 30px;
-    place-items: center;
-  }
+main {
+   max-width: 1200px;
+   margin: 0 auto;
+   display: grid;
+   grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+   ;
+   gap: 30px;
+   place-items: center;
+}
 
+#reset {
+   text-align: center;
+   cursor: pointer;
+}
 </style>
 
 <h1>Memory Cards</h1>
-<main>
-{#each arr as card, i}
+<main id="main">
+    {#each arr as card, i}
     <Card
-      title={i + 1}
-      image={`https://source.unsplash.com/random/260x200?${card}?${theme ? theme : ''}`}
-      onClick={cardClickHandler}
-      active={openCards.includes(i + 1) || firstChoice.innerText == i + 1 || secondChoice.innerText == i + 1} />
-  {/each}
-  <p>Guesses: {guesses}</p>
-  <p>Matches: {matches}</p>
-  <p>Percentage: {guesses != 0 ? Math.round((matches * 100) / guesses) : 0}%</p>
-  <p>{message}</p>
+        title={i + 1}
+        image={`https://source.unsplash.com/random/260x200?${card}?${theme ? theme : ''}`}
+        onClick={cardClickHandler}
+        active={openCards.includes(i + 1) || firstChoice.innerText == i + 1 || secondChoice.innerText == i + 1} />
+    {/each}
+    <p>Guesses: {guesses}</p>
+    <p>Matches: {matches}</p>
+    <p>Percentage: {guesses != 0 ? Math.round((matches * 100) / guesses) : 0}%</p>
+    <p>{message}</p>
+
 </main>
+{#if message=="You won!"}
+<p id="reset"on:click={resetGame}>Reset</p>
+{/if}
--- src/Card.svelte
+++ src/Card.svelte
@@ -1,56 +1,58 @@
 <script>
-  export let title, image, onClick, active;
+export let title, image, onClick, active;
 </script>
 
 <style>
-  .flip-card {
-    background-color: transparent;
-    width: 260px;
-    height: 200px;
-    perspective: 1000px;
-    cursor: pointer;
-  }
+.flip-card {
+   background-color: transparent;
+   width: 260px;
+   height: 200px;
+   perspective: 1000px;
+   cursor: pointer;
+}
 
-  .flip-card-inner {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    transition: transform 0.5s;
-    transform-style: preserve-3d;
-  }
+.flip-card-inner {
+   position: relative;
+   width: 100%;
+   height: 100%;
+   text-align: center;
+   transition: transform 0.5s;
+   transform-style: preserve-3d;
+}
 
-  .active {
-    transform: rotateY(180deg);
-  }
-  
-  .flip-card-front,
-  .flip-card-back {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-  }
-  .flip-card-front {
-    background-color: dodgerblue;
-    background:url('https://source.unsplash.com/random/260x200?white');
-    color: white;
-    display: grid;
-    place-items: center;
-  }
-  .flip-card-back {
-    transform: rotateY(180deg);
-    background-color: #bbb;
-    color: black;
-  }
+.active {
+   transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+   position: absolute;
+   width: 100%;
+   height: 100%;
+   -webkit-backface-visibility: hidden;
+   backface-visibility: hidden;
+}
+
+.flip-card-front {
+   background-color: dodgerblue;
+   background: url('https://source.unsplash.com/random/260x200?white');
+   color: white;
+   display: grid;
+   place-items: center;
+}
+
+.flip-card-back {
+   transform: rotateY(180deg);
+   background-color: #bbb;
+   color: black;
+}
 </style>
 
-<div class="flip-card" on:click={onClick}>
-  <div class={!active ? 'flip-card-inner' : 'flip-card-inner active'}>
-    <div class="flip-card-front">
-      <h1>{title}</h1>
+<div class="flip-card" on:click={onClick} >
+    <div class={!active ? 'flip-card-inner' : 'flip-card-inner active'}>
+        <div class="flip-card-front">
+            <h1>{title}</h1>
+        </div>
+        <div class="flip-card-back"><img class="image" src={image} alt="alt" /></div>
     </div>
-    <div class="flip-card-back"><img src={image} alt="alt" /></div>
-  </div>
 </div>
